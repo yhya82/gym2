ca914dfe9d6c5698dbcfb2a6ad566fa7d2d31896
yhya82 <gassamay78@mail.com>
2026-08-14 16:44:09 -0700

    Cron fixed

--- app/Console/Commands/ExpireMemberships.php
+++ app/Console/Commands/ExpireMemberships.php
@@ -36,9 +36,7 @@ class ExpireMemberships extends Command
      */
     public function handle(): int
     {
-        \Log::info('memberships:expire scheduler ran', [
-    'time' => now()->toDateTimeString(),
-]);
+      
         $today = now()->toDateString();
         $expiredCount = 0;
 
